e97d83d6065b8fce83aa672161c734f59d122ba5
VovaCrafter2005 <vova.crafter2005@gmail.com>
2016-02-14 16:09:49 +0300

    Поправил расположение элементов в умножении и прозрачность кнопок

--- приложение умножение/ViewController.m
+++ приложение умножение/ViewController.m
@@ -24,10 +24,11 @@
     [super viewDidLoad];
     
     self.smileImageView.alpha = 0;
-    
+    self.nextView.alpha = 0;
     [self.rezultat becomeFirstResponder];
     [self randomNumbers];
     [self loadRecord];
+
 }
 
 - (void)randomNumbers {
@@ -40,6 +41,10 @@
 
 - (IBAction)pokazatOtvet:(id)sender {
     self.rezultat.text = [NSString stringWithFormat:@"%ld",(long)self.oneChislo * self.twoChislo];
+    self.proverit.alpha = 0;
+    self.otvetView.alpha = 0;
+    self.nextView.alpha = 1;
+
 }
 
 - (IBAction)proverit:(id)sender {
@@ -48,27 +53,24 @@
         self.repet.text = @"верно, молодец!";
         self.smileImageView.image = [UIImage imageNamed:@"smile!.png"];
         self.smileImageView.alpha = 1;
-
-        [UIView animateWithDuration:1
-                              delay:0
-                            options:UIViewAnimationOptionCurveEaseInOut
-                         animations:^{
-                             self.nextView.center = CGPointMake(self.nextView.center.x, self.nextView.center.y - 60);
-//                             self.otvetView.center = CGPointMake(self.otvetView.center.x, self.otvetView.center.y + 60);
-//                             self.proverit.center = CGPointMake(self.proverit.center.x, self.proverit.center.y + 60);
-                         } completion:^(BOOL finished) {
-                         }];
+        self.proverit.alpha = 0;
+        self.otvetView.alpha = 0;
+        self.nextView.alpha = 1;
+        
+        
+//        [UIView animateWithDuration:1
+//                              delay:0
+//                            options:UIViewAnimationOptionCurveEaseInOut
+//                         animations:^{
+//                             self.nextView.center = CGPointMake(self.nextView.center.x, self.nextView.center.y - 60);
+////                             self.otvetView.center = CGPointMake(self.otvetView.center.x, self.otvetView.center.y + 60);
+////                             self.proverit.center = CGPointMake(self.proverit.center.x, self.proverit.center.y + 60);
+//                         } completion:^(BOOL finished) {
+//                         }];
 
         self.chet123++;
         self.chet.text = [NSString stringWithFormat:@"%ld", self.chet123];
         
-        NSUserDefaults* userDefaults = [NSUserDefaults standardUserDefaults];
-        if (self.chet123 > [userDefaults integerForKey:@"Рекорд верных ответов"]) {
-            [userDefaults setInteger:self.chet123 forKey:@"Рекорд верных ответов"];
-            [userDefaults synchronize];
-            [self loadRecord];
-            self.chet.text = [NSString stringWithFormat:@"%ld", (long)self.chet123];
-        }
     } else{
        self.rezultatIgroka = [self.rezultat.text intValue] ;
         if (self.rezultatIgroka != self.oneChislo * self.twoChislo) {
@@ -85,6 +87,9 @@
     self.rezultat.text = @"";
     [self randomNumbers];
     self.smileImageView.alpha = 0;
+    self.proverit.alpha = 1;
+    self.otvetView.alpha = 1;
+    self.nextView.alpha = 0;
 }
 
 
