d0669f94c53ce90c6ea1753c02912601d9f8360a
VovaCrafter2005 <vova.crafter2005@gmail.com>
2016-02-22 18:12:00 +0300

    создал массив для умной обработке

--- приложение умножение/ViewController.m
+++ приложение умножение/ViewController.m
@@ -10,11 +10,14 @@
 
 @interface ViewController ()
 
+@property (strong, nonatomic) NSMutableArray* arrayTableAll;
+
 @property (assign, nonatomic) NSInteger oneChislo;
 @property (assign, nonatomic) NSInteger twoChislo;
 @property (assign, nonatomic) NSInteger rezultatIgroka;
 @property (assign, nonatomic) NSInteger chet123;
 @property (assign, nonatomic) NSInteger antiChet123;
+@property (assign, nonatomic) BOOL repeatedError;
 
 @end
 
@@ -23,6 +26,7 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     
+    self.repeatedError = NO;
     self.smileImageView.alpha = 0;
     self.nextView.alpha = 0;
     [self.rezultat becomeFirstResponder];
@@ -31,9 +35,45 @@
 
 }
 
+- (void)initArrayTable {
+    
+    NSMutableArray* arrayTableAll = [[NSMutableArray alloc] initWithObjects:[NSNumber init],
+                                                                            [NSNumber init],
+                                                                            [NSNumber init],
+                                                                            [NSNumber init],
+                                                                            [NSNumber init],
+                                                                            [NSNumber init],
+                                                                            [NSNumber init],
+                                                                            [NSNumber init],
+                                                                            [NSNumber init],
+                                                                            [NSNumber init],
+                                                                            [NSNumber init],
+                                                                            [NSNumber init], nil];
+    
+    self.arrayTableAll = arrayTableAll;
+    
+    int i;
+    for (i = 0; i <= 10; i++) {
+        NSMutableArray* arrayTable = [[NSMutableArray alloc] initWithObjects:[NSNumber numberWithInteger:0],
+                                                                            [NSNumber numberWithInteger:0],
+                                                                            [NSNumber numberWithInteger:0],
+                                                                            [NSNumber numberWithInteger:0],
+                                                                            [NSNumber numberWithInteger:0],
+                                                                            [NSNumber numberWithInteger:0],
+                                                                            [NSNumber numberWithInteger:0],
+                                                                            [NSNumber numberWithInteger:0],
+                                                                            [NSNumber numberWithInteger:0],
+                                                                            [NSNumber numberWithInteger:0],
+                                                                            [NSNumber numberWithInteger:0], nil];
+        [arrayTableAll addObject:arrayTable];
+    }
+
+}
+
 - (void)randomNumbers {
-    self.oneChislo = arc4random() % 4 + 2;
-    self.twoChislo = arc4random() % 9 + 2;
+
+    self.oneChislo = arc4random() % 4 + 3;
+    self.twoChislo = arc4random() % 9 + 1;
 
     self.oneNumber.text = [NSString stringWithFormat:@"%ld", (long)self.oneChislo];
     self.twoNumber.text = [NSString stringWithFormat:@"%ld",(long)self.twoChislo];
@@ -41,42 +81,59 @@
 
 - (IBAction)pokazatOtvet:(id)sender {
     self.rezultat.text = [NSString stringWithFormat:@"%ld",(long)self.oneChislo * self.twoChislo];
-    self.proverit.alpha = 0;
-    self.otvetView.alpha = 0;
-    self.nextView.alpha = 1;
+
+    
+    [UIView animateWithDuration:0.3
+                          delay:0
+                        options:UIViewAnimationOptionCurveEaseInOut
+                     animations:^{
+                         self.proverit.alpha = 0;
+                         self.otvetView.alpha = 0;
+                         self.nextView.alpha = 1;
+                     } completion:^(BOOL finished) {
+                     }];
 
 }
 
 - (IBAction)proverit:(id)sender {
     self.rezultatIgroka = [self.rezultat.text intValue] ;
     if (self.rezultatIgroka == self.oneChislo * self.twoChislo) {
+        
         self.repet.text = @"верно, молодец!";
         self.smileImageView.image = [UIImage imageNamed:@"smile!.png"];
         self.smileImageView.alpha = 1;
-        self.proverit.alpha = 0;
-        self.otvetView.alpha = 0;
-        self.nextView.alpha = 1;
-        
-        
-//        [UIView animateWithDuration:1
-//                              delay:0
-//                            options:UIViewAnimationOptionCurveEaseInOut
-//                         animations:^{
-//                             self.nextView.center = CGPointMake(self.nextView.center.x, self.nextView.center.y - 60);
-////                             self.otvetView.center = CGPointMake(self.otvetView.center.x, self.otvetView.center.y + 60);
-////                             self.proverit.center = CGPointMake(self.proverit.center.x, self.proverit.center.y + 60);
-//                         } completion:^(BOOL finished) {
-//                         }];
+
+        [UIView animateWithDuration:0.3
+                              delay:0
+                            options:UIViewAnimationOptionCurveEaseInOut
+                         animations:^{
+                             self.proverit.alpha = 0;
+                             self.otvetView.alpha = 0;
+                             self.nextView.alpha = 1;
+                         } completion:^(BOOL finished) {
+                         }];
 
         self.chet123++;
         self.chet.text = [NSString stringWithFormat:@"%ld", self.chet123];
-        
-    } else{
-       self.rezultatIgroka = [self.rezultat.text intValue] ;
-        if (self.rezultatIgroka != self.oneChislo * self.twoChislo) {
-            self.repet.text = @"неправельно,попробуй еще раз";
+        self.repeatedError = NO;
+    } else {
+        if (self.repeatedError) {
+            [UIView animateWithDuration:1
+                                  delay:0
+                                options:UIViewAnimationOptionCurveEaseInOut
+                             animations:^{
+                                 self.repet.textColor = [UIColor redColor];
+                             } completion:^(BOOL finished) {
+                                 self.repet.textColor = [UIColor blackColor];
+                             }];
+            self.repet.text = @"Неправельно!";
+            self.repeatedError = NO;
+
+        } else {
+            self.repet.text = @"Неправельно, попробуй еще раз";
             self.smileImageView.image = [UIImage imageNamed:@"smile..png"];
             self.smileImageView.alpha = 1;
+            self.repeatedError = YES;
         }
     }
 }
@@ -87,9 +144,16 @@
     self.rezultat.text = @"";
     [self randomNumbers];
     self.smileImageView.alpha = 0;
-    self.proverit.alpha = 1;
-    self.otvetView.alpha = 1;
-    self.nextView.alpha = 0;
+    
+    [UIView animateWithDuration:0.3
+                          delay:0
+                        options:UIViewAnimationOptionCurveEaseInOut
+                     animations:^{
+                         self.proverit.alpha = 1;
+                         self.otvetView.alpha = 1;
+                         self.nextView.alpha = 0;
+                     } completion:^(BOOL finished) {
+                     }];
 }
 
 
